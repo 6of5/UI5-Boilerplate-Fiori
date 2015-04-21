c404a96a4a30ec73443925ab0a6fe302b9f9751f
hpseitz <information@6of5.com>
2015-04-21 21:13:51 +0200

    improvements

--- Component.js
+++ Component.js
@@ -3,112 +3,119 @@ jQuery.sap.require("ui5bp.app.config");
 jQuery.sap.require("sap.m.routing.RouteMatchedHandler");
 
 sap.ui.core.UIComponent.extend("ui5bp.Component", {
-	metadata: {
-		"name": "UI5 Boilerplate Fiori",
-		"version": ui5bp.app.config.APP_VERSION,
-		"includes": ["css/style.css"],
-		"dependencies": {
-			"libs": ["sap.m"],
-			"components": []
-		},
+    metadata: {
+        "name": "UI5 Boilerplate Fiori",
+        "version": ui5bp.app.config.APP_VERSION,
+        "includes": ["css/style.css"],
+        "dependencies": {
+            "libs": ["sap.m"],
+            "components": []
+        },
 
-		"config": {
-			"resourceBundle": "i18n/i18n.properties",
-			"titleResource": "xtit.shellTitle",
-			"favIcon": "img/favicon.ico",
-			"phone": "img/57_iPhone_Desktop_Launch.png",
-			"phone@2": "img/114_iPhone-Retina_Web_Clip.png",
-			"tablet": "img/72_iPad_Desktop_Launch.png",
-			"tablet@2": "img/144_iPad_Retina_Web_Clip.png",
-			"serviceConfig": {}
-		},
+        "config": {
+            "resourceBundle": "i18n/i18n.properties",
+            "titleResource": "xtit.shellTitle",
+            "favIcon": "img/favicon.ico",
+            "phone": "img/57_iPhone_Desktop_Launch.png",
+            "phone@2": "img/114_iPhone-Retina_Web_Clip.png",
+            "tablet": "img/72_iPad_Desktop_Launch.png",
+            "tablet@2": "img/144_iPad_Retina_Web_Clip.png",
+            "serviceConfig": {}
+        },
 
-		rootView: {
-			viewName: "ui5bp.view.App",
-			type: "JS"
-		},
-		routing: {
-			config: {
-				viewType: "JS",
-				viewPath: "ui5bp.view", // common prefix
-				targetAggregation: "detailPages",
-				targetControl: "idAppControl",
-				clearTarget: false
-			},
-			routes: [{
-					pattern: "Launchpad",
-					name: "Launchpad",
-					view: "Launchpad",
-					viewLevel: 0,
-					targetAggregation: "detailPages"
-			}, {
-					pattern: "",
-					name: "Menu",
-					view: "Menu",
-					viewLevel: 0,
-					targetAggregation: "masterPages"
+        rootView: {
+            viewName: "ui5bp.view.App",
+            type: "JS"
+        },
+        routing: {
+            config: {
+                viewType: "JS",
+                viewPath: "ui5bp.view", // common prefix
+                targetAggregation: "detailPages",
+                targetControl: "idAppControl",
+                clearTarget: false
+            },
+            routes: [{
+                pattern: "Launchpad",
+                name: "Launchpad",
+                view: "Launchpad",
+                viewLevel: 1,
+                targetAggregation: "detailPages"
+            }, {
+                pattern: "",
+                name: "Menu",
+                view: "Menu",
+                viewLevel: 1,
+                targetAggregation: "masterPages"
+            }]
+        }
+    },
 
-			}
-			]
-		}
-	},
+    init: function() {
+        sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
 
-	init: function() {
-		sap.ui.core.UIComponent.prototype.init.apply(this, arguments);
+        // always use absolute paths relative to our own component
+        // (relative paths will fail if running in the Fiori Launchpad)
+        var rootPath = jQuery.sap.getModulePath("ui5bp");
 
-		// always use absolute paths relative to our own component
-		// (relative paths will fail if running in the Fiori Launchpad)
-		var rootPath = jQuery.sap.getModulePath("ui5bp");
+        // set i18n model
+        var i18nModel = new sap.ui.model.resource.ResourceModel({
+            bundleUrl: rootPath + "/i18n/i18n.properties"
+        });
+        this.setModel(i18nModel, "i18n");
 
-		// set i18n model
-		var i18nModel = new sap.ui.model.resource.ResourceModel({
-			bundleUrl: rootPath + "/i18n/i18n.properties"
-		});
-		this.setModel(i18nModel, "i18n");
+        this._oRouteMatchedHandler = new sap.m.routing.RouteMatchedHandler(this.getRouter());
+        var router = this.getRouter();
 
-		this._oRouteMatchedHandler = new sap.m.routing.RouteMatchedHandler(this.getRouter());
-		var router = this.getRouter();
-		router.initialize();
-
-		//load routes from menu.json
+        //load routes from menu.json
         var model = new sap.ui.model.json.JSONModel(rootPath + "/model/menu.json");
         model.attachRequestCompleted(null, function() {
-            var data = null, m = 0, menu = null, routeConfig = null;
+            var data = null,
+                m = 0,
+                menu = null,
+                routeConfig = null;
             data = model.getData();
             if (data && data.Menu) {
                 for (m = 0; m < data.Menu.length; m++) {
                     menu = data.Menu[m];
                     routeConfig = {
-                    	name : menu.targetPage,
-                    	pattern : menu.targetPage,
-                    	view : menu.targetPage,
-                    	viewLevel : 1
+                        name: menu.targetPage,
+                        pattern: menu.targetPage,
+                        view: menu.targetPage,
+                        viewLevel: 2
                     };
-                    if(menu.viewType){
-                    	routeConfig.viewType = menu.viewType;
+                    if (menu.viewType) {
+                        routeConfig.viewType = menu.viewType;
                     }
-                    if(menu.pattern){
-                    	routeConfig.pattern = menu.pattern;
+                    if (menu.pattern) {
+                        routeConfig.pattern = menu.pattern;
                     }
-                    if(menu.targetAggregation){
-                    	routeConfig.targetAggregation = menu.targetAggregation;
+                    if (menu.targetAggregation) {
+                        routeConfig.targetAggregation = menu.targetAggregation;
                     }
-                    if(menu.viewLevel){
-                    	routeConfig.viewLevel = menu.viewLevel;
-                    }                    
-                    if(menu.subroutes){
-                    	routeConfig.subroutes = menu.subroutes;
+                    if (menu.viewLevel) {
+                        routeConfig.viewLevel = menu.viewLevel;
+                    }
+                    if (menu.subroutes) {
+                        routeConfig.subroutes = menu.subroutes;
                     }
                     router.addRoute(routeConfig);
-                }
+                } //for
+            } //if
+
+            router.initialize();
+
+            if (ui5bp.app.config.LaunchpadMode) {
+                router.navTo("Launchpad");
+            } else {
+                router.navTo("Menu");
             }
         });
+    },
 
-        if(ui5bp.app.config.LaunchpadMode){
-            router.navTo("Launchpad");
-        } else {
-            router.navTo("Menu");            
-        }		
-	}
+    destroy: function() {
+        this._oRouteMatchedHandler.destroy();
+        sap.ui.core.UIComponent.prototype.destroy.apply(this, arguments);
+    }
 
 });
--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# UI5 Boilerplate - Fiori Edition
+# UI5 Boilerplate - Fiori Edition (UI5BP Fiori)
 
 UI5 Boilerplate - Fiori Edition is a template for building Apps based on OpenUI5 or SAPUI5, which can be integrated into the SAP Fiori Launchpad. 
 The UI5 Boilerplate uses the sap.m.SplitApp which itself is especially designed for tablet usage (iPad), but is also usable on smartphones with iOS and Android (iPhone 4s/5/5s/6, Nexus 4/5, ....). 
@@ -16,6 +16,11 @@ More Info can be found here:
 * Follow UI5 Boilerplate on Twitter: [@UI5bp](http://twitter.com/UI5bp)
 * Contacts: Twitter [@hpseitz](http://twitter.com/hpseitz) or Email to information@6of5.com
 
+## Posts
+* SAP Inside Track Frankfurt 2014 (sitFRA) - Birthday of UI5BP Fiori Edition: [http://blog.mypro.de/2015/04/17/sitfra-2015-birthday-ui5bp-fiori-edition/](http://blog.mypro.de/2015/04/17/sitfra-2015-birthday-ui5bp-fiori-edition/)
+* Deploy UI5 Boilerplate on Fiori Launchpad of HANA Cloud Platform: [http://scn.sap.com/community/fiori/blog/2015/04/14/deploy-ui5-boilerplate-on-fiori-launchpad-of-hana-cloud-platform](http://scn.sap.com/community/fiori/blog/2015/04/14/deploy-ui5-boilerplate-on-fiori-launchpad-of-hana-cloud-platform)
+* Extend Fiori UI5 Boilerplate with SAP Web IDE (add further view's) [http://scn.sap.com/community/fiori/blog/2015/04/20/extend-fiori-ui5-boilerplate-with-sap-web-ide](http://scn.sap.com/community/fiori/blog/2015/04/20/extend-fiori-ui5-boilerplate-with-sap-web-ide)
+
 ## Features
 * Based on OpenUI5, more Infos under [http://sap.github.io/openui5/](http://sap.github.io/openui5/)
 * Based on sap.m library of UI5, which provide touch optimized controls
@@ -25,8 +30,14 @@ More Info can be found here:
 * Navigation via LeftMenu navigation (default) or Launchpad of App itself 
 * Automatic generation of navigation items (Launchpad or LeftMenu) and routing.
 * i18n base model
+* Fiori compliant (hopefully)
+
+## Screenshot
+integrated into SAP Fiori Launchpad on HANA Cloud Platform
+![ui5bp fiori screenhot](http://blog.mypro.de/wp-content/uploads/2015/04/UI5BP-31-UI5BP-Fiori-in-FLP.png)
 
 ## License
 Apache License, Version 2.0
 
 ![ui5 Logo](http://blog.mypro.de/wp-content/uploads/2014/01/ui5_144.jpg)
+
